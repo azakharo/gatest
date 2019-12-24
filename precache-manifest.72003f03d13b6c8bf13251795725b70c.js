1f8a51a6c911247ff68dac6bbaa60f44930fedf1
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2019-12-24 20:10:39 +0300

    Updates

---
+++ precache-manifest.72003f03d13b6c8bf13251795725b70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66f6f62fad9b880ae69f80632f34496",
+    "url": "/gatest/index.html"
+  },
+  {
+    "revision": "60e1b1ae7c068d32b4cd",
+    "url": "/gatest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "430a8bbe12c0bdc57a42",
+    "url": "/gatest/static/js/2.2897d2c7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/gatest/static/js/2.2897d2c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60e1b1ae7c068d32b4cd",
+    "url": "/gatest/static/js/main.3178d9ee.chunk.js"
+  },
+  {
+    "revision": "ec0e38ab23f4bb890a30",
+    "url": "/gatest/static/js/runtime-main.83f13671.js"
+  }
+]);
