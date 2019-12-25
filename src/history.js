8d6f07a4eaf8c8ad5e0c3fe90c16dc6d0d538e0f
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2019-12-25 11:09:23 +0300

    temporarily disable GA

--- src/history.js
+++ src/history.js
@@ -1,17 +1,21 @@
 import {createBrowserHistory as createHistory} from 'history';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-154998375-1');
+console.log('init');
 
 const history = createHistory();
 
-console.log('init');
+const isProduction = false;
 
-// Listen for changes to the current location
-history.listen((location, action) => {
-  // location is an object like window.location
-  console.log(action, location);
-  ReactGA.pageview(location.pathname + location.search);
-});
+if (isProduction) {
+  ReactGA.initialize('UA-154998375-1');
+
+  // Listen for changes to the current location
+  history.listen((location, action) => {
+    // location is an object like window.location
+    console.log(action, location);
+    ReactGA.pageview(location.pathname + location.search);
+  });
+}
 
 export default history;
