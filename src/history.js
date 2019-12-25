68c0eb6c4b103587b125ea7588b2da265757cde8
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2019-12-25 11:14:27 +0300

    enabled GA again

--- src/history.js
+++ src/history.js
@@ -1,21 +1,15 @@
 import {createBrowserHistory as createHistory} from 'history';
 import ReactGA from 'react-ga';
 
-console.log('init');
-
 const history = createHistory();
 
-const isProduction = false;
+ReactGA.initialize('UA-154998375-1');
 
-if (isProduction) {
-  ReactGA.initialize('UA-154998375-1');
-
-  // Listen for changes to the current location
-  history.listen((location, action) => {
-    // location is an object like window.location
-    console.log(action, location);
-    ReactGA.pageview(location.pathname + location.search);
-  });
-}
+// Listen for changes to the current location
+history.listen((location, action) => {
+  // location is an object like window.location
+  console.log(action, location);
+  ReactGA.pageview(location.pathname + location.search);
+});
 
 export default history;
